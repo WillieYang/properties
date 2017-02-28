b528f21106408b09b7fc2c405478401cf7c7244a
WillieYang <849346604@qq.com>
2017-02-28 22:50:15 +0000

    Import Item in the spider and save it to various formats...xml, json, csv

--- properties/spiders/basic.py
+++ properties/spiders/basic.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from properties.items import PropertiesItem
 
 class BasicSpider(scrapy.Spider):
     name = "basic"
@@ -8,18 +8,21 @@ class BasicSpider(scrapy.Spider):
     start_urls = ['https://www.gumtree.com/flats-houses']
 
     def parse(self, response):
-        self.log("title: %s" % response.xpath(
-        	'//*[@itemprop="name"][1]/text()').extract())
+    	item = PropertiesItem()
 
-        self.log("price: %s" % response.xpath(
-        	'//*[@itemprop="price"][1]/text()').re('[.0-9]+'))
+        item['title'] = response.xpath(
+        	'//*[@itemprop="name"][1]/text()').extract()
 
-        self.log("description: %s" % response.xpath(
-        	'//*[@itemprop="description"][1]/text()').extract())
+        item['price'] = response.xpath(
+        	'//*[@itemprop="price"][1]/text()').re('[.0-9]+')
 
-        self.log("address: %s" % response.xpath(
+        item['description'] = response.xpath(
+        	'//*[@itemprop="description"][1]/text()').extract()
+
+        item['address'] = response.xpath(
         	'//*[@itemtype="http://schema.org/'
-        	'Place"][1]/text()').extract())
+        	'Place"][1]/text()').extract()
 
-        self.log("image_urls: %s" % response.xpath(
-        	'//*[@itemprop="image"][1]/@src').extract())
+        item['image_urls'] = response.xpath(
+        	'//*[@itemprop="image"][1]/@src').extract()
+        return item
