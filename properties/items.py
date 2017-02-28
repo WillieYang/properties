7c3da63a9b6b12aae3ec59761bf1059d97f2584f
WillieYang <849346604@qq.com>
2017-02-28 17:18:08 +0000

    Create a Spider file

--- properties/items.py
+++ properties/items.py
@@ -5,10 +5,23 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class PropertiesItem(scrapy.Item):
+class PropertiesItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = Field()
+    price = Field()
+    description = Field()
+    address = Field()
+    image_urls = Field()
+
+    images = Field()
+    location = Field()
+
+    url = Field()
+    project = Field()
+    spider = Field()
+    server = Field()
+    date = Field()
