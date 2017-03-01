410ffebc10b94d7463bffaae087110c41d62ac29
WillieYang <849346604@qq.com>
2017-03-01 22:06:22 +0000

    Make use of processors and Itemloaders, ps contract check

--- properties/spiders/basic.py
+++ properties/spiders/basic.py
@@ -1,28 +1,48 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from properties.items import PropertiesItem
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose, Join
+import urlparse
+import datetime
+import socket
 
 class BasicSpider(scrapy.Spider):
     name = "basic"
     allowed_domains = ["web"]
-    start_urls = ['https://www.gumtree.com/flats-houses']
+    start_urls = ['https://www.gumtree.com/property-for-sale']
 
     def parse(self, response):
-    	item = PropertiesItem()
 
-        item['title'] = response.xpath(
-        	'//*[@itemprop="name"][1]/text()').extract()
+    	""" This function parses a property page.
+    	@url https://www.gumtree.com/flats-houses
+    	@returns items 1
+    	@scrapes title price description address image_urls
+    	@scrapes url project spider server date
+		"""
 
-        item['price'] = response.xpath(
-        	'//*[@itemprop="price"][1]/text()').re('[.0-9]+')
+    	l = ItemLoader(item = PropertiesItem(), response=response)
+    	
+    	l.add_xpath('title', '//*[@itemprop="name"][1]/text()',
+    				MapCompose(unicode.strip, unicode.title))
 
-        item['description'] = response.xpath(
-        	'//*[@itemprop="description"][1]/text()').extract()
+    	l.add_xpath('price', './/*[@itemprop="listing_price"][1]/text()',
+    		MapCompose(lambda i: i.replace(',', ''), float),
+    		re='[,.0-9]+')
 
-        item['address'] = response.xpath(
-        	'//*[@itemtype="http://schema.org/'
-        	'Place"][1]/text()').extract()
+    	l.add_xpath('description', '//*[@itemprop="description"]'
+    		'[1]/text()', MapCompose(unicode.strip), Join())
 
-        item['image_urls'] = response.xpath(
-        	'//*[@itemprop="image"][1]/@src').extract()
-        return item
+    	l.add_xpath('address', '//*[@itemtype="http://schema.org/Product"]'
+    		'[1]/text()', MapCompose(unicode.strip))
+
+    	l.add_xpath('image_urls', '//*[@itemprop="image"]'
+    		'[1]/@src', MapCompose(lambda i: urlparse.urljoin(response.url, i)))
+
+    	l.add_value('url', response.url)
+    	l.add_value('project', self.settings.get('BOT_NAME'))
+    	l.add_value('spider', self.name)
+    	l.add_value('server', socket.gethostname())
+    	l.add_value('date', datetime.datetime.now())
+
+    	return l.load_item()
